92747d928729e540458035392efe7b2912b81844
Sayed Abdo <sayedabdo@Sayeds-Mac.local>
2018-03-03 12:23:55 -0800

    Create A Group VC :) :)

---
+++ BreakPoint/View/UserCell.swift
@@ -0,0 +1,42 @@
+//
+//  UserCell.swift
+//  BreakPoint
+//
+//  Created by Sayed Abdo on 3/3/18.
+//  Copyright © 2018 sayedAbdo. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    var showing = false
+    
+    func configureCell(profileImage image: UIImage, email: String, isSelected: Bool) {
+        self.profileImage.image = image
+        self.emailLbl.text = email
+        if isSelected {
+            self.checkImage.isHidden = false
+        } else {
+            self.checkImage.isHidden = true
+        }
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected {
+            if showing == false {
+                checkImage.isHidden = false
+                showing = true
+            } else {
+                checkImage.isHidden = true
+                showing = false
+            }
+        }
+    }
+}
+
